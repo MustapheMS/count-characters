277dff34033bd4732c33f9cd7958b8b1eb322914
MustapheMS <moztaphem9@gmail.com>
2023-06-15 20:38:50 +0300

    Count String Done

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Good Luck 💪🏾
+
+//Done 💯💯😎😎
+
+
+function count(num, string) 
+{
+    let count = 0
+    for (let m = 0; m < string.length; m++) 
+    {
+      if (string[m] == num) 
+      {
+        count++;
+      }
+    }
+    return count;
+  }
+let leter = "c"
+let sentence = "fikrcamp coding Bootcamp"
+let result = count(leter, sentence)
+    console.log(result)
